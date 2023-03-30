b5bb573951f40917428df5ac3844a6f2f6d95a62
mariuslbraun <marius.braun-2@wirtschaft.uni-giessen.de>
2023-03-30 16:29:33 +0200

    refactored parameter space generation

--- scripts/paramspaces.R
+++ scripts/paramspaces.R
@@ -1,4 +1,4 @@
-# This R code generates random draws from a +-10 % interval around the 
+# This R code generates random draws from a +-10% interval around the 
 # elasticities of substitution esubd(i) for each sector in the Pothen and
 # Huebler model (2018). It then writes these draws to a CSV file to be 
 # used as the parameter space for sensitivity analysis in Snakemake.
@@ -8,23 +8,55 @@
 library(readr)
 library(dplyr)
 
-numDraws = 1000  # number of draws from interval
+rm(list = ls())
+
+num_draws = 1000  # number of draws from interval
 set.seed(127)   # set random seed
+variation = 0.1
+num_decimals = 3
 
-# import CSV file with intervals
-intervals_10p = read.csv("C:\\Users\\Marius Braun\\Sensitivity analyses\\esubd(i)2\\intervals_esubd(i)_10_percent.csv", header = TRUE)
-n = nrow(intervals_10p)
-
-numbers = data.frame(matrix(nrow = numDraws, ncol = 0)) # generate empty data frame to store random draws in
-
-for(i in 1:n){
-  # draws from a uniform distribution of +- 10 % around each estimate
-  numbers = cbind(numbers, round(runif(numDraws, min = intervals_10p[i, 2], max = intervals_10p[i, 3]), 3))
+# create vector of elasticity names
+elasticities = c("esubd", "esubm", "esubva")
+for(i in 1:length(elasticities)) {
+  # load CSV file with baseline parameter values
+  filename = paste("intervals_10p", elasticities[i], sep = "_")
+  file = read.csv(
+    paste0("paramspaces/", elasticities[i], "_baseline.csv"),
+    header = T
+  )
+  # vary baseline values by +- 10%
+  file$elasticity_minus_10p = file$elasticity * (1 - variation)
+  file$elasticity_plus_10p = file$elasticity * (1 + variation)
+  
+  assign(
+    x = filename,
+    value = file
+  )
+  
+  num_sectors = nrow(get(filename))
+  paramspace = data.frame(matrix(nrow = num_draws, ncol = 0))
+  
+  # generate draws from a uniform distribution of +- 10 % around each estimate
+  for(j in 1:num_sectors){
+    paramspace = cbind(
+      paramspace, 
+      round(
+        runif(
+        num_draws,
+        min = (get(filename))["elasticity_minus_10p"][j, ],
+        max = (get(filename))["elasticity_plus_10p"][j, ]),
+        num_decimals
+        )
+      )
+  }
+  
+  # write parameter space to CSV file
+  colnames(paramspace) = (get(filename))$X...sector
+  write.csv(
+    paramspace,
+    paste0("paramspaces/paramspace_", elasticities[i], ".csv"),
+    row.names = FALSE
+    )
 }
-remove(i)
-
-colnames(numbers) = c("dAGR", "dCOA", "dCRU", "dNGA", "dPET", "dFOO", "dMIN", "dPAP", "dCHE", "dNMM", "dIRS", "dNFM", "dMAN", "dELE", "dTRN", "dCON", "dSER")
-
-# write random draws to CSV file as parameter space
-write.csv(numbers, "C:\\Users\\Marius Braun\\Sensitivity analyses\\esubd(i)2\\parameter_space_esubd(i)_interval_10p.csv", row.names = FALSE)
+rm(i, j, filename, file, paramspace)
   
